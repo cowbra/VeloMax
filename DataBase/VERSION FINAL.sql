89dfcf34072e984fbcbd4625fb1e1c3c8ddddb35
cowbra <hugosanchez1303@gmail.com>
2022-04-27 21:45:08 +0200

    Update VERSION FINAL.sql

--- DataBase/VERSION FINAL.sql
+++ DataBase/VERSION FINAL.sql
@@ -1,7 +1,9 @@
+--SUPPRESSION DE LA BDD SI EXISTANTE PUIS CREATION
 DROP DATABASE IF EXISTS `VeloMax` ;
 CREATE DATABASE `VeloMax`;
 USE `VeloMax`;
 
+--CREATION DE NOS TABLES
 CREATE TABLE `BICYCLETTE` (
  `ID_Bicyclette` int(30) NOT NULL AUTO_INCREMENT,
  `Nom_Bicyclette` varchar(255) NOT NULL,
@@ -27,7 +29,7 @@ CREATE TABLE `FOURNISSEUR` (
  `NomEntreprise_Fournisseur` varchar(255) NOT NULL,
  `Contact_Fournisseur` varchar(255) NOT NULL,
  `Adresse_Fournisseur` varchar(255) NOT NULL,
- `Libelle_Fournisseur` varchar(255) NOT NULL,
+ `Libelle_Fournisseur` enum('1','2','3','4') NOT NULL,
  PRIMARY KEY (`Siret_Fournisseur`)
 );
 
@@ -104,3 +106,21 @@ CREATE TABLE `FOURNIT` (
  FOREIGN KEY (`NumProduit_Piece`) REFERENCES `PIECE` (`NumProduit_Piece`),
  FOREIGN KEY (`Siret_Fournisseur`) REFERENCES `FOURNISSEUR` (`Siret_Fournisseur`)
 );
+
+--INSERTION DES VALEURS
+INSERT INTO `FIDELIO` (`NumProgramme_Fidelio`, `Description_Fidelio`, `Cout_Fidelio`, `Duree_Fidelio`, `Rabais_Fidelio`) VALUES
+(1, 'Fidélio', 15, 1, 0.05),
+(2, 'Fidélio Or', 25, 2, 0.08),
+(3, 'Fidélio Platine', 60, 2, 0.1),
+(4, 'Fidélio Max', 100, 3, 0.12);
+
+INSERT INTO `FOURNISSEUR` (`Siret_Fournisseur`, `NomEntreprise_Fournisseur`, `Contact_Fournisseur`, `Adresse_Fournisseur`, `Libelle_Fournisseur`) VALUES
+(39982698100017, 'BricoVelo', 'bricovelo@velo.com', '14 rue Victor Hugo, Argentan', '1'),
+(67822698100036, 'VeloFabrik', 'velofabrik@gmail.com', '4 rue Carnot, Lille', '3'),
+(43282695524021, 'Bikeshop', 'bikeshop@gmail.com', '6 rue André, Paris', '12'),
+(27382698187536, 'PiecesFabrique', 'piecesfabrique@gmail.com', '9 avenue Richelieu, Caen', '4');
+
+INSERT INTO `CLIENT` (`ID_Client`, `Type_Client`, `Tel_Client`, `Courriel_Client`, `Adresse_Client`, `Nom_Client`, `Prenom_Client`, `NomCompagnie_Client`, `RemiseCompagnie_Client`, `NumProgramme_Fidelio`, `DateDebut_Fidelio`, `DateFin_Fidelio`) VALUES
+(1, 'Particulier', '0685324585', 'hugo@test.fr', '41 rue Jobin\r\n13003 Marseille', 'bob', 'hugo', NULL, NULL, 1, '2022-04-07', NULL),
+(2, 'Particulier', '0632544489', 'rufus@gmail.com', '19 rue des Augustins\r\n33000 Bordeaux', 'Chemine', 'Rufus', NULL, NULL, 4, '2022-04-14', NULL),
+(3, 'Entreprise', '0147855472', 'Dekatlon@free.fr', '108, rue de rhodes\r\n34000 Montpellier', 'Ben', NULL, 'RentaBikke', 0.1, NULL, NULL, NULL);
