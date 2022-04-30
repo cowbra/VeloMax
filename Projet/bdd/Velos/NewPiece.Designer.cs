9c145a91b1692f81d0f7340283d5ebe283c5af9f
cowbra <32939303+cowbra@users.noreply.github.com>
2022-04-30 15:47:08 +0200

    velos 1

---
+++ Projet/bdd/Velos/NewPiece.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace bdd.Velos
+{
+    partial class NewPiece
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.checkedListBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Sitka Subheading", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(850, 562);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(744, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Est-ce une pièce existante mais fournie par un nouveau Fournisseur ou est-ce une " +
+    "toute nouvelle Pièce ?";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(42, 60);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(333, 27);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Pièce existante via un nouveau Fournisseur";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(455, 60);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(173, 27);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Toute nouvelle pièce";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Le type de la Pièce";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(349, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "La Date d\'introduction de la Pièce sur le marché";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Cadre",
+            "Guidon",
+            "Freins",
+            "Selle",
+            "Dérailleur Avant",
+            "Dérailleur Arrière",
+            "Roue Avant",
+            "Roue Arrière",
+            "Réflecteurs",
+            "Pédalier",
+            "Ordinateur",
+            "Panier"});
+            this.checkedListBox1.Location = new System.Drawing.Point(379, 145);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(249, 27);
+            this.checkedListBox1.TabIndex = 6;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker1.Location = new System.Drawing.Point(379, 193);
+            this.dateTimePicker1.MinDate = new System.DateTime(1950, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(260, 25);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Sitka Subheading", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(6, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Indiquez :";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker2.Location = new System.Drawing.Point(379, 245);
+            this.dateTimePicker2.MinDate = new System.DateTime(1950, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(260, 25);
+            this.dateTimePicker2.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(289, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "La Date de fin de production de la Pièce";
+            // 
+            // NewPiece
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 679);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "NewPiece";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Label label1;
+        private CheckedListBox checkedListBox1;
+        private Label label3;
+        private Label label2;
+        private DateTimePicker dateTimePicker2;
+        private Label label5;
+        private Label label4;
+        private DateTimePicker dateTimePicker1;
+        private BindingSource bindingSource1;
+    }
+}
--- Projet/bdd/Velos/Piece.cs
+++ Projet/bdd/Velos/Piece.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
 
 namespace bdd
 {
     public class Piece
     {
+        #region Attributs
+        protected string typePiece;
+        protected string dateIntroduction;
+        protected string dateFin;
+
+        #endregion
+
+        // Constructeur client particulier
+        public Piece(string typePiece, string dateIntroduction, string dateFin)
+        {
+            this.typePiece = typePiece;
+            this.dateIntroduction = dateIntroduction;
+            this.dateFin = dateFin;
+        }
+
+        public bool AddToBdd()
+        {
+            BDD DATABASE = new BDD();
+            DATABASE.Connect();
+            if (DATABASE.Connected == true)
+            {
+                MySqlCommand requete = new MySqlCommand("INSERT INTO PIECE(Description_Piece,DateDebut_Piece,DateFin_Piece) VALUES(@Description_Piece,@DateDebut_Piece,@DateFin_Piece)", DATABASE.MySqlConnection);
+                requete.Parameters.AddWithValue("@Description_Piece", this.typePiece);
+                requete.Parameters.AddWithValue("@DateDebut_Piece", this.dateIntroduction);
+                requete.Parameters.AddWithValue("@DateFin_Piece", this.dateFin);
+
+                requete.ExecuteNonQuery();
+                requete.Parameters.Clear();
+
+                DATABASE.Disconnect();
+                return true;
+            }
+            return false;
+        }
+
+
     }
 }
