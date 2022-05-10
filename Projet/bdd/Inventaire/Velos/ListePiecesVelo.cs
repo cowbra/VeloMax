a09d0f1252d925a8e94149dc96e125ceb9cb32e5
cowbra <hugosanchez1303@gmail.com>
2022-05-10 16:14:33 +0200

    Menu velo creation

---
+++ Projet/bdd/Inventaire/Velos/ListePiecesVelo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace bdd
+{
+    public partial class ListePiecesVelo : Form
+    {
+        BDD DATABASE = new BDD();
+        public ListePiecesVelo(string idBicyclette)
+        {
+            InitializeComponent();
+            DATABASE.Connect();
+        }
+
+        private void ListePiecesVelo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Fill(string id)
+        {
+            string sql ="SELECT * FROM PIECE WHERE Identifiant_Piece ='" + id + "'";
+        }
+    }
+}
--- Projet/bdd/Inventaire/Velos/Velo.cs
+++ Projet/bdd/Inventaire/Velos/Velo.cs
@@ -14,24 +14,11 @@ namespace bdd
         protected string dateIntro;
         protected string dateFin;
 
-        protected string cadre;
-        protected string guidon;
-        protected string? freins;
-        protected string selle;
-        protected string? derailleurAvant;
-        protected string? derailleurArriere;
-        protected string roueAvant;
-        protected string roueArriere;
-        protected string? reflecteurs;
-        protected string pedalier;
-        protected string? ordinateur;
-        protected string? panier;
         #endregion
 
 
         #region Constructeur
-        public Velo(string nom, string grandeur, double prix, string typeVelo, string dateIntro, string dateFin, string cadre, string guidon, string freins, string selle,
-            string derailleurAvant, string derailleurArriere, string roueAvant, string roueArriere, string reflecteurs, string pedalier, string ordinateur, string panier)
+        public Velo(string nom, string grandeur, double prix, string typeVelo, string dateIntro, string dateFin)
         {
             this.nom = nom;
             this.grandeur = grandeur;
@@ -40,19 +27,6 @@ namespace bdd
             this.dateIntro = dateIntro;
             this.dateFin = dateFin;
 
-
-            this.cadre = cadre;
-            this.guidon = guidon;
-            this.freins = freins;
-            this.selle = selle;
-            this.derailleurAvant = derailleurAvant;
-            this.derailleurArriere = derailleurArriere;
-            this.roueAvant = roueAvant;
-            this.roueArriere = roueArriere;
-            this.reflecteurs = reflecteurs;
-            this.ordinateur = ordinateur;
-            this.panier = panier;
-            this.pedalier = pedalier;
 
         }
         #endregion
@@ -63,33 +37,21 @@ namespace bdd
             DATABASE.Connect();
             if (DATABASE.Connected == true)
             {
+                #region Creation du modele de Bicyclette
                 MySqlCommand requete = new MySqlCommand("INSERT INTO BICYCLETTE(Nom_Bicyclette,Grandeur_Bicyclette,Prix_Bicyclette,Type_Bicyclette,DateIntroduction_Bicyclette,DateFin_Bicyclette) VALUES(@Nom_Bicyclette,@Grandeur_Bicyclette,@Prix_Bicyclette,@Type_Bicyclette,@DateIntroduction_Bicyclette,@DateFin_Bicyclette)", DATABASE.MySqlConnection);
                 requete.Parameters.AddWithValue("@Nom_Bicyclette", this.nom);
                 requete.Parameters.AddWithValue("@Grandeur_Bicyclette", this.grandeur);
                 requete.Parameters.AddWithValue("@Prix_Bicyclette", this.prix);
                 requete.Parameters.AddWithValue("@Type_Bicyclette", this.typeVelo);
                 requete.Parameters.AddWithValue("@DateIntroduction_Bicyclette", this.dateIntro);
-                requete.Parameters.AddWithValue("@DateFin_Bicyclette", this.dateIntro);
+                requete.Parameters.AddWithValue("@DateFin_Bicyclette", this.dateFin);
 
                 requete.ExecuteNonQuery();
                 requete.Parameters.Clear();
-
-                //ON RECUPERE L'ID DU DERNIER MODELE DE BICYCLETTE AJOUTEE
-                string idBicyclette = "";
-                requete = new MySqlCommand("SELECT LAST_INSERT_ID() FROM BICYCLETTE", DATABASE.MySqlConnection);
-                using (MySqlDataReader Lire = requete.ExecuteReader())
-                {
-                    while (Lire.Read())
-                    {
-                        idBicyclette = Lire.GetString(0);
-                    }
-                }
+                #endregion
 
 
-
-
-
-                //DATABASE.Disconnect();
+                DATABASE.Disconnect();
                 return true;
             }
             return false;
